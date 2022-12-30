2fcc9e16c588a6e60ef35c98eda8a869253ebc57
jnbjensen <mail@jnbjensen.com>
2022-12-30 14:49:01 +0100

    Add HTML and basic layout for ActiveProgramPage

--- src/components/ActiveProgramPage.js
+++ src/components/ActiveProgramPage.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Styled } from './ActiveProgramPage.styled';
 
 const ActiveProgramPage = () => {
   return (
-    <h1>Active Program Page!</h1>
+    <Styled.Wrapper>
+      <div>
+        <button type="button">PROFILE</button>
+        <button type="button">LOG OUT</button>
+      </div>
+      <div>
+        <h2>You&apos;re on DAY 2 of your happiness initiative!</h2>
+      </div>
+      <Styled.ChallengeContainer>
+        <div>
+          <h3>Completed challenges</h3>
+        </div>
+        <div>
+          <h3>Today&apos;s challenge</h3>
+        </div>
+      </Styled.ChallengeContainer>
+    </Styled.Wrapper>
   )
 }
 
